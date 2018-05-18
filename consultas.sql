033bb685a7a7221de019bb1c2abb8e2fdd6633e8
Juan-Arteaga <36280899+Juan-Arteaga@users.noreply.github.com>
2018-05-18 11:11:23 -0500

    grupo 7

---
+++ consultas.sql
@@ -0,0 +1,13 @@
+SELECT * 
+FROM  `animales-extintos` 
+LIMIT 0 , 30
+
+
+muestra las columnas que se encuentra en la tabla animales-extintos
+
+SELECT * 
+FROM  `animales-extintos` 
+WHERE nombre =  "perezoso grande colombiano"
+LIMIT 0 , 30
+
+muestra la busqueda que se encuentra en la columna nombre 
